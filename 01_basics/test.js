93c6f77a0698cc64eeeeff855b903a5ba34ccb28
brijeshgupta0001 <123300580+brijeshgupta0001@users.noreply.github.com>
2024-06-28 10:42:25 +0000

    basic

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -7,9 +7,22 @@
 // console.log(a);
 // console.log(null);
 
-const id = Symbol('123');
-const userid = Symbol('123');
-console.log(id == userid);
-console.log(typeof id);
-console.log(id);
-console.log(userid);
+// const id = Symbol('123');
+// const userid = Symbol('123');
+// console.log(id == userid);
+// console.log(typeof id);
+// console.log(id);
+// console.log(userid);
+
+const RandomNumber = 8764.65774
+console.log(RandomNumber.toFixed(2));
+console.log(RandomNumber.toPrecision(4));
+console.log(RandomNumber.toPrecision(6));
+console.log(RandomNumber.toString().length);
+
+console.table([Math.round(RandomNumber),Math.ceil(RandomNumber),Math.floor(RandomNumber)])
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
