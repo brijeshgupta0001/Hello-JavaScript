7e6a1e4e009747b21223cbde2f0ecdc5005a4020
brijeshgupta0001 <123300580+brijeshgupta0001@users.noreply.github.com>
2024-06-28 08:18:32 +0000

    basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,15 @@
-console.log("Hello Mister")
-console.log("Nothing")
+// console.log("Hello Mister")
+// console.log("Nothing")
+// console.table([typeof(NaN),typeof(undefined),typeof(null)]);
+// console.table(["hello","world","jhvj"]);
+// let a = null;
+// console.log(typeof(a));
+// console.log(a);
+// console.log(null);
+
+const id = Symbol('123');
+const userid = Symbol('123');
+console.log(id == userid);
+console.log(typeof id);
+console.log(id);
+console.log(userid);
